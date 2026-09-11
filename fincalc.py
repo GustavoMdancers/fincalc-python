69ddb94428b9481e28bba57791866012e2cae068
Mateus0062 <mateusmendmattos@gmail.com>
2026-09-11 10:14:40 -0300

    adição da feature de calcular irrf

--- fincalc.py
+++ fincalc.py
@@ -24,6 +24,17 @@ def calcular_juros_compostos(capital: float, taxa_anual: float, anos: int) -> fl
     montante = capital * ((1 + (taxa_anual / 100)) ** anos)
     return montante
 
+# Implementação da Feature Cálculo de IRRF - Mateus Mendes Mattos
+def calcular_irrf(salario_bruto: float) -> float:
+    if salario_bruto <= 2259.20:
+        return 0.0
+    elif salario_bruto <= 2826.65:
+        return (salario_bruto * 0.075) - 169.44
+    elif salario_bruto <= 3751.05:
+        return (salario_bruto * 0.15) - 381.44
+    else:
+        return (salario_bruto * 0.225) - 662.77
+
 
 if __name__ == "__main__":
     print("Iniciando o sistema FinCalc...")
@@ -33,3 +44,8 @@ if __name__ == "__main__":
     print(f"Juros Compostos: R$ {montante_comp:.2f}")
     patrimonio = calcular_aposentadoria(10000.0, 500.0, 20, 6.0)
     print(f"Patrimônio Estimado para Aposentadoria: R$ {patrimonio:.2f}")
+
+    # Exibindo o cálculo do IRRF para um salário bruto de R$ 3000,00 - Mateus Mendes Mattos
+    irrf = calcular_irrf(3000.00)
+    print(f"Calculo da aliquota simplificada de Imposto de Renda Retido na Fonte: R$ {irrf:.2f}")
+    
